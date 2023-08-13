7d702db23e0ac653014a5ec37f16e3e6d3a2e7b1
charlesjohn101 <85096052+charlesjohn101@users.noreply.github.com>
2023-08-13 14:42:52 +0530

    app.py

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ import streamlit as st
 st.title("Stock Input")
 
 
+# Create a radio switch
+selected_option = st.radio("Trade type", ["Buy on dip", "Short - Sell"])
+
+st.write("")
+
+
 col1, col2, col3, col4 = st.columns(4)
 
 with col1:
@@ -19,8 +25,6 @@ with col3:
 with col4:
     sell_level = st.selectbox("sell level", list(range(1, 5)))
 
-import streamlit as st
-import streamlit as st
 
 q_init = 10
 
@@ -31,34 +35,73 @@ rupees_symbol = "\u20B9"
 
 new_cmp = selected_stock
 gap = selected_gap
-av_sum = 0
-av_count = 0
-sell_lev = []
 
-st.write("")
-st.write("")
-for i in range(buy_level):
-    st.markdown(f'<h5 style="color: grey;">{q_init} @ {round(new_cmp, 2)}</h5>', unsafe_allow_html=True)
-    sell_lev.append(new_cmp)
-    av_sum = av_sum + (q_init * new_cmp)
-    av_count = av_count + q_init
-    new_cmp = new_cmp - gap
-    q_init = q_init + 10
 
-average = av_sum / av_count
-profit = sell_lev[sell_level - 1] - average
 
-st.write("")
 
-st.markdown(f'<h6 style="color: black; margin-bottom: 0px;">Total Amount: {av_sum:,.2f}</h6>', unsafe_allow_html=True)
-st.markdown(f'<h6 style="color: black; margin-bottom: 0px;">Total Quantity: {av_count:,.2f}</h6>', unsafe_allow_html=True)
+# Display the selected option
+if selected_option=="Buy on dip":
+    av_sum = 0
+    av_count = 0
+    sell_lev = []
 
-st.write("")
+    st.write("")
+    st.write("")
+    for i in range(buy_level):
+        st.markdown(f'<h5 style="color: grey;">{q_init} @ {round(new_cmp, 2)}</h5>', unsafe_allow_html=True)
+        sell_lev.append(new_cmp)
+        av_sum = av_sum + (q_init * new_cmp)
+        av_count = av_count + q_init
+        new_cmp = new_cmp - gap
+        q_init = q_init + 10
 
-col1, col2, col3 = st.columns(3)
-with col1:
-    st.markdown(f'<h5 style="color: black; margin-bottom: 0px;">Average price: {rupees_symbol}{average:,.2f}</h5>', unsafe_allow_html=True)
-with col2:
-    st.markdown(f'<h5 style="color: black; margin-bottom: 0px;">Profit per share:{rupees_symbol} {profit:,.2f}</h5>', unsafe_allow_html=True)
-with col3:
-    st.markdown(f'<h5 style="color: black; margin-bottom: 0px;">Total Profit: {rupees_symbol}{(profit * av_count):,.2f}</h5>', unsafe_allow_html=True)
+    average = av_sum / av_count
+    profit = sell_lev[sell_level - 1] - average
+
+    st.write("")
+
+    st.markdown(f'<h6 style="color: black; margin-bottom: 0px;">Total Amount: {av_sum:,.2f}</h6>', unsafe_allow_html=True)
+    st.markdown(f'<h6 style="color: black; margin-bottom: 0px;">Total Quantity: {av_count:,.2f}</h6>', unsafe_allow_html=True)
+
+    st.write("")
+
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.markdown(f'<h5 style="color: black; margin-bottom: 0px;">Average price: {rupees_symbol}{average:,.2f}</h5>', unsafe_allow_html=True)
+    with col2:
+        st.markdown(f'<h5 style="color: black; margin-bottom: 0px;">Profit per share:{rupees_symbol} {profit:,.2f}</h5>', unsafe_allow_html=True)
+    with col3:
+        st.markdown(f'<h5 style="color: black; margin-bottom: 0px;">Total Profit: {rupees_symbol}{(profit * av_count):,.2f}</h5>', unsafe_allow_html=True)
+else:
+    av_sum = 0
+    av_count = 0
+    sell_lev = []
+
+    st.write("")
+    st.write("")
+    for i in range(buy_level):
+        st.markdown(f'<h5 style="color: grey;">{q_init} @ {round(new_cmp, 2)}</h5>', unsafe_allow_html=True)
+        sell_lev.append(new_cmp)
+        av_sum = av_sum + (q_init * new_cmp)
+        av_count = av_count + q_init
+        new_cmp = new_cmp + gap
+        q_init = q_init + 10
+
+    average = av_sum / av_count
+    profit = average - sell_lev[sell_level - 1]
+
+    st.write("")
+
+    st.markdown(f'<h6 style="color: black; margin-bottom: 0px;">Total Amount: {av_sum:,.2f}</h6>', unsafe_allow_html=True)
+    st.markdown(f'<h6 style="color: black; margin-bottom: 0px;">Total Quantity: {av_count:,.2f}</h6>', unsafe_allow_html=True)
+
+    st.write("")
+
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.markdown(f'<h5 style="color: black; margin-bottom: 0px;">Average price: {rupees_symbol}{average:,.2f}</h5>', unsafe_allow_html=True)
+    with col2:
+        st.markdown(f'<h5 style="color: black; margin-bottom: 0px;">Profit per share:{rupees_symbol} {profit:,.2f}</h5>', unsafe_allow_html=True)
+    with col3:
+        st.markdown(f'<h5 style="color: black; margin-bottom: 0px;">Total Profit: {rupees_symbol}{(profit * av_count):,.2f}</h5>', unsafe_allow_html=True)
+
